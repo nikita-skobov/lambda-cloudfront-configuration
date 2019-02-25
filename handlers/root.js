b6e11034729ffaafec0d7cd49abc6fa3e937b2bf
nikita-skobov <nskobov.websites@gmail.com>
2019-02-25 16:32:50 -0600

    adds basic root handler

---
+++ handlers/root.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line
+'use strict';
+
+module.exports.handler = async (event, context) => {
+  let statusCode = 500
+  let body = { error: 'Unable to issue signature' }
+  let headers = {
+    'Content-Type': 'application/json',
+    // USE one of the following for CORS. either a specific domain, or all domains
+    'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    // 'Access-Control-Allow-Origin': `https://${process.env.DOMAIN}.com`, // Required for CORS support to work
+
+    // USE the following if you have a private API that needs an authorization header
+    // 'Access-Control-Allow-Credentials': true, // Required for authorization headers with HTTPS
+
+    // This function generates a unique signature, so you probably don't need caching...
+    // 'Cache-Control': 'max-age=2',
+  }
+
+  try {
+    // TODO: implement your own code
+
+    statusCode = 200
+    body = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    </head>
+    <body>
+    <p>test, test, test</p>
+    </body>
+    </html>
+    `
+    headers['Content-Type'] = 'text/html'
+  } catch (e) {
+    console.log(e)
+    headers = e.headers || headers
+    statusCode = e.statusCode || statusCode
+    body = e.body || body
+  }
+
+  return {
+    // if there was an error, and we return application/json, make sure to stringify
+    // otherwise just return the body as is.
+    body: headers['Content-Type'] === 'application/json' ? JSON.stringify(body) : body,
+    statusCode,
+    headers,
+  }
+}
